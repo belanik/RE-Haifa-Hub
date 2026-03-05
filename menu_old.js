9733a4b1fd54bdadf614aeaef524137d8fb8805b
belanik <156224673+belanik@users.noreply.github.com>
2026-03-05 23:29:49 +0200

    Delete menu_old.js

--- menu_old.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-  const menu = document.getElementById("sideMenu");
-  if (menu.style.width === "250px" || menu.style.width === "220px") {
-    menu.style.width = "0";
-  } else {
-    menu.style.width = "250px";
-  }
-}
