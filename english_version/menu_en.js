b16775ec31cbc0c5adde013858e88a59bfc4a676
belanik <156224673+belanik@users.noreply.github.com>
2026-09-08 20:48:07 +0300

    Update menu_en.js

--- english_version/menu_en.js
+++ english_version/menu_en.js
@@ -61,7 +61,10 @@
   nav.className = "top-nav";
   nav.setAttribute("aria-label", "Navigation");
   nav.innerHTML = `
-    <a href="/index_en.html">News</a>
+   
+
+    <a href="/english_version/index_en.html">News</a>
+    
   
     <a href="/english_version/position_on_waste_reform_en.html" style="color:#ffd200;">Position on Waste Reform</a>
 
